4e3c6f948184b237ce1d577448996c9af02cfb8d
toby lorne <toby@toby.codes>
2019-08-04 10:49:35 +0100

    remove ruby source code
    Signed-off-by: toby lorne <toby@toby.codes>

--- fetcher/spec/photographers_gallery_fetcher_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative "#{PROJECT_PATH}/src/photographers_gallery_fetcher"
-
-context 'Photographers Gallery Fetcher' do
-  it 'fetches the events' do
-    events = PhotographersGalleryFetcher.fetch
-
-    expect(events.length).to_not eq(0)
-  end
-end
