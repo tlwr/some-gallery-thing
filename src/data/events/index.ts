80a2c2a162588215efbe98bdb74a771daee3cce8
toby lorne <toby@toby.codes>
2019-08-04 13:56:20 +0100

    Add some gallery images to exampleEvents
    Signed-off-by: toby lorne <toby@toby.codes>

--- src/data/events/index.ts
+++ src/data/events/index.ts
@@ -13,7 +13,9 @@ export const exampleEvents: ReadonlyArray<GalleryEvent> = [
     gallery: {
       name: 'Tate Modern',
       address: 'Bankside, London SE1 9TG',
-      website: 'https://www.tate.org.uk/visit/tate-modern'
+      website: 'https://www.tate.org.uk/visit/tate-modern',
+
+      image: 'https://storage.googleapis.com/tate-digital/ui/3.9.0-compressed/static/images/tate-logo.png',
     },
 
     image: 'https://www.tate.org.uk/sites/default/files/styles/width-600/public/van_gogh_self_portrait.jpg',
@@ -28,7 +30,7 @@ export const exampleEvents: ReadonlyArray<GalleryEvent> = [
     gallery: {
       name: 'Victoria Miro',
       address: '16 Wharf Road, London N1 7RW',
-      website: 'https://www.victoria-miro.com'
+      website: 'https://www.victoria-miro.com',
     },
 
     image: 'https://artlogic-res.cloudinary.com/w_1600,h_1000,c_limit,f_auto,fl_lossy/ws-victoriamiro/usr/exhibitions/images/548/cp201-image-3-.jpg',
@@ -43,7 +45,9 @@ export const exampleEvents: ReadonlyArray<GalleryEvent> = [
     gallery: {
       name: 'Dulwich Picture Gallery',
       address: 'Gallery Road, London SE21 7AD',
-      website: 'https://www.dulwichpicturegallery.org.uk'
+      website: 'https://www.dulwichpicturegallery.org.uk',
+
+      image: 'https://www.dulwichpicturegallery.org.uk/assets/img/logo.png',
     },
 
     image: 'https://www.dulwichpicturegallery.org.uk/media/9413/tube-train-power-large-banner.jpg',
