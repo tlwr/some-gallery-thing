e321afc10f042db5960e06ccac034bf4df3ad965
toby lorne <toby@toby.codes>
2019-08-04 13:48:45 +0100

    Add GalleryEvent images to exampleEvents
    Signed-off-by: toby lorne <toby@toby.codes>

--- src/data/events/index.ts
+++ src/data/events/index.ts
@@ -15,6 +15,8 @@ export const exampleEvents: ReadonlyArray<GalleryEvent> = [
       address: 'Bankside, London SE1 9TG',
       website: 'https://www.tate.org.uk/visit/tate-modern'
     },
+
+    image: 'https://www.tate.org.uk/sites/default/files/styles/width-600/public/van_gogh_self_portrait.jpg',
   },
   {
     title: 'Celia Paul',
@@ -28,6 +30,8 @@ export const exampleEvents: ReadonlyArray<GalleryEvent> = [
       address: '16 Wharf Road, London N1 7RW',
       website: 'https://www.victoria-miro.com'
     },
+
+    image: 'https://artlogic-res.cloudinary.com/w_1600,h_1000,c_limit,f_auto,fl_lossy/ws-victoriamiro/usr/exhibitions/images/548/cp201-image-3-.jpg',
   },
   {
     title: 'Cutting Edge: Modernist British Printmaking',
@@ -41,5 +45,7 @@ export const exampleEvents: ReadonlyArray<GalleryEvent> = [
       address: 'Gallery Road, London SE21 7AD',
       website: 'https://www.dulwichpicturegallery.org.uk'
     },
+
+    image: 'https://www.dulwichpicturegallery.org.uk/media/9413/tube-train-power-large-banner.jpg',
   },
 ];
