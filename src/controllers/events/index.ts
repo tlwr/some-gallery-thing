cb355fa3c9ade70e87480af595407346077ce2f7
toby lorne <toby@toby.codes>
2019-08-04 13:38:15 +0100

    Use exampleEvents in /events
    Signed-off-by: toby lorne <toby@toby.codes>

--- src/controllers/events/index.ts
+++ src/controllers/events/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
+import {exampleEvents} from '../../data/events';
+
 export const listEvents = (
   _req: express.Request,
   res: express.Response,
 ) => {
   res.render('events/list.njk', {
-    events: [],
+    events: exampleEvents,
   })
 };
