c4982ba24f32a8ee105f891f6d9d0377ac21a083
toby lorne <toby@toby.codes>
2019-10-06 21:47:15 +0100

    Close metatag and use camelcase attr for TSX
    Signed-off-by: toby lorne <toby@toby.codes>

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -12,7 +12,7 @@ export class GalleryEventsComponent extends React.Component<GalleryEventsCompone
     return <html lang="en">
       <head>
         <title>some gallery thing</title>
-        <meta charset="UTF-8">
+        <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/>
         <link rel="stylesheet" type="text/css" href="/assets/main.css"/>
