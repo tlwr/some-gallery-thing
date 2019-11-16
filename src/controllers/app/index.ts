dedde367af29c7ce2238ac6e6a8895730149813d
toby lorne <toby@toby.codes>
2019-11-16 21:44:36 +0000

    Remove healthchecks
    We don't need it because CF workers
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- src/controllers/app/index.ts
+++ src/controllers/app/index.ts
@@ -3,28 +3,22 @@ import URI from 'urijs';
 
 import {EventsController} from '../events';
 import {AssetsController} from '../assets';
-import {HealthcheckController} from '../healthcheck';
 
 import {GalleryCollector} from '../../types';
 
 export class AppController {
   private assetsController: AssetsController;
   private eventsController: EventsController;
-  private healthcheckController: HealthcheckController;
 
   public constructor(collector: GalleryCollector) {
     this.assetsController = new AssetsController();
     this.eventsController = new EventsController(collector);
-    this.healthcheckController = new HealthcheckController();
   }
 
   public async handle(req: Request): Promise<Response> {
     switch(new URI(req.url).pathname()) {
       case '/events':
         return this.eventsController.handleListEvents(req);
-
-      case '/healthcheck':
-        return this.healthcheckController.handle(req);
 
       case '/assets/main.css':
         return this.assetsController.handleCSS(req);
--- src/controllers/healthcheck/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "node-fetch";
-
-export class HealthcheckController {
-  public async handle(req: Request): Promise<Response> {
-    return new Response('OK', { headers: { 'Content-Type': 'text/plain' } });
-  }
-}
