15b56b24df5fd09e02a8323cdd52139b68e7cac1
toby lorne <toby@toby.codes>
2020-09-22 21:47:40 +0100

    tests: simplify integration tests
    this integration test is very fragile, because individual galleries
    sometimes go offline, they will just not be collected during compile
    time
    
    if we don't collect _many_ events, then we should worry
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- src/integration-tests/index.test.ts
+++ src/integration-tests/index.test.ts
@@ -2,68 +2,12 @@ import collectors from '../collectors';
 
 describe('integration-tests', () => {
   describe('collectors', () => {
-    it('should collect events from Dulwich Picture Gallery', async () => {
-      const events = await collectors.DulwichPictureGallery();
+    it('should collect some events', async () => {
+      const events = await collectors.All.collect();
 
       console.info(events);
 
-      expect(events.length).toBeGreaterThan(0);
-    });
-
-    it('should collect events from Gasworks', async () => {
-      const events = await collectors.Gasworks();
-
-      console.info(events);
-
-      expect(events.length).toBeGreaterThan(0);
-    });
-
-    it('should collect events from the Tintype Gallery', async () => {
-      const events = await collectors.TintypeGallery();
-
-      console.info(events);
-
-      expect(events.length).toBeGreaterThan(0);
-    });
-
-    it('should collect events from Tate Modern & Tate Britain', async () => {
-      const events = await collectors.Tate();
-
-      console.info(events);
-
-      expect(events.length).toBeGreaterThan(0);
-
-      expect(
-        events.filter(e => e.gallery.name.match(/tate modern/i)).length,
-      ).toBeGreaterThan(0)
-
-      expect(
-        events.filter(e => e.gallery.name.match(/tate britain/i)).length,
-      ).toBeGreaterThan(0)
-    });
-
-    it('should collect events from The Photographers Gallery', async () => {
-      const events = await collectors.ThePhotographersGallery();
-
-      console.info(events);
-
-      expect(events.length).toBeGreaterThan(0);
-    });
-
-    it.skip('should collect events from White Cube', async () => {
-      const events = await collectors.WhiteCube();
-
-      console.info(events);
-
-      expect(events.length).toBeGreaterThan(0);
-    });
-
-    it('should collect events from Whitechapel Gallery', async () => {
-      const events = await collectors.WhitechapelGallery();
-
-      console.info(events);
-
-      expect(events.length).toBeGreaterThan(0);
+      expect(events.length).toBeGreaterThan(8);
     });
   });
 });
