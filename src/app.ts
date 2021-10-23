f30681345effd603663444f0977572d8097dde29
toby lorne <toby@toby.codes>
2021-10-23 10:56:21 +0200

    controllers: app controller is superfluous
    we can move this into the express app entrypoint because the code is
    different for cf workers
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import URI from 'urijs';
 import { Request, Response } from "node-fetch";
 
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-var-requires
 const pino: any = require('express-pino-logger');
 
-import {AppController} from './controllers/app';
+import {AssetsController} from './controllers/assets';
+import {EventsController} from './controllers/events';
+
 import collectors from './collectors';
 import {isProduction} from './utils';
 
@@ -18,12 +21,27 @@ const collector = isProduction()
   ? /* istanbul ignore next */collectors.All
   : collectors.Stub;
 
-const appController = new AppController(collector);
+const assetsController = new AssetsController();
+const eventsController = new EventsController(collector);
 
 app.get('/*', async (req: express.Request, res: express.Response) => {
   const fullURL = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-  const response = await appController.handle(new Request(fullURL));
+  const request = new Request(fullURL);
+  let response : Response;
+
+  switch(new URI(req.url).pathname()) {
+    case '/assets/main.css':
+      response = await assetsController.handleCSS(request);
+      break;
+
+    case '/events':
+      response = await eventsController.handleListEvents(request);
+      break;
+
+    default:
+      response = new Response('', {status: 302, headers: { 'Location': '/events' }});
+  }
 
   for (const header in response.headers.raw()) {
     res.set(header, response.headers.get(header));
