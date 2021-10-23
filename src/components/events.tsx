acf8098686b85a3991e3dff1e277b040dc7bb8ff
toby lorne <toby@toby.codes>
2021-10-23 19:42:09 +0200

    ui: favicon using svg hack
    https://css-tricks.com/emojis-as-favicons/
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -27,6 +27,7 @@ export class GalleryEventsComponent extends React.Component<GalleryEventsCompone
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/>
         <link rel="stylesheet" type="text/css" href="/assets/main.css"/>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖼️</text></svg>"/>
       </head>
       <body>
 
